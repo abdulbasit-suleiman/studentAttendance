79fbf94f73c8237636f6815cffcba67acf1c50c6
abdulbasit-suleiman <abdulbasitsuleiman28@gmail.com>
2023-11-04 19:58:09 +0100

    new changes

---
+++ pages/studentsPage/signUp.js
@@ -0,0 +1,125 @@
+import { useEffect,useState } from "react";
+function SignUp() {
+  let faceio;
+  // useEffect(()=>{
+  //   faceio= new faceio('xxxxxxxx')
+  // }, [])
+  // const [payload, setPayload]=useState({
+  //   userEmail:'',
+  //   pin:'',
+  // })
+  // const onChange=(e)=>{
+  //   setPayload({
+  //     ...payload,
+  //     [e.target.name]:e.target.value,
+  //   })
+  // }
+  // const handleSubmit=(e)=>{
+  //   // prevent refresh
+  //   e.preventDefault()
+  //   handlSignIn(payload)
+  // }
+  
+  return (
+    <div className="signUp">
+      <div className="signUpHeader">
+        <h3>Sign Up</h3>
+        <img src="../asset/logo.svg" />
+        <h4 className="bold-text">Student Attendance</h4>
+      </div>
+      <div className="signUpForm">
+        <form>
+          <label for="fullName">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12.0009 17C15.6633 17 18.8659 18.5751 20.608 20.9247L18.766 21.796C17.3482 20.1157 14.8483 19 12.0009 19C9.15346 19 6.6535 20.1157 5.23577 21.796L3.39453 20.9238C5.13673 18.5747 8.33894 17 12.0009 17ZM12.0009 2C14.7623 2 17.0009 4.23858 17.0009 7V10C17.0009 12.6888 14.8786 14.8818 12.2178 14.9954L12.0009 15C9.23945 15 7.00087 12.7614 7.00087 10V7C7.00087 4.31125 9.12318 2.11818 11.784 2.00462L12.0009 2ZM12.0009 4C10.4032 4 9.09721 5.24892 9.00596 6.82373L9.00087 7V10C9.00087 11.6569 10.344 13 12.0009 13C13.5986 13 14.9045 11.7511 14.9958 10.1763L15.0009 10V7C15.0009 5.34315 13.6577 4 12.0009 4Z"
+                fill="#00923F"
+              />
+            </svg>
+            Full Name (Surname in capital letters)
+          </label>
+          <input type="text" placeholder="Enter Full Name" />
+
+          <label for="matricNo">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12.0009 17C15.6633 17 18.8659 18.5751 20.608 20.9247L18.766 21.796C17.3482 20.1157 14.8483 19 12.0009 19C9.15346 19 6.6535 20.1157 5.23577 21.796L3.39453 20.9238C5.13673 18.5747 8.33894 17 12.0009 17ZM12.0009 2C14.7623 2 17.0009 4.23858 17.0009 7V10C17.0009 12.7614 14.7623 15 12.0009 15C9.23945 15 7.00087 12.7614 7.00087 10V7C7.00087 4.23858 9.23945 2 12.0009 2Z"
+                fill="#00923F"
+              />
+            </svg>
+            Matric Number
+          </label>
+          <input type="text" placeholder="Enter your matric no" />
+
+          <label for="schoolmailAcc">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z"
+                fill="#00923F"
+              />
+            </svg>
+            School Mail Account
+          </label>
+          <input type="email" placeholder=" Enter School Account" />
+
+          <label for="college">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 18.4853L16.2426 14.2426C18.5858 11.8995 18.5858 8.10051 16.2426 5.75736C13.8995 3.41421 10.1005 3.41421 7.75736 5.75736C5.41421 8.10051 5.41421 11.8995 7.75736 14.2426L12 18.4853ZM17.6569 15.6569L12 21.3137L6.34315 15.6569C3.21895 12.5327 3.21895 7.46734 6.34315 4.34315C9.46734 1.21895 14.5327 1.21895 17.6569 4.34315C20.781 7.46734 20.781 12.5327 17.6569 15.6569ZM5 22H19V24H5V22Z"
+                fill="#00923F"
+              />
+            </svg>
+            College
+          </label>
+          <input type="text" placeholder=" Enter your College" />
+
+          <label for="department">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z"
+                fill="#00923F"
+              />
+            </svg>
+            Department
+          </label>
+          <input type="text" placeholder=" Enter your Department" />
+
+       <div className="signUpHeader"> 
+       <button className="save">Save info</button>
+       </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default SignUp;
---
+++ pages/studentsPage/studentSplash.js
@@ -0,0 +1,10 @@
+function Splash(){
+    return(
+        <div className="splash">
+<img src='../asset/logo.svg' />
+
+<h4 className="bold-text" >Student Attendance</h4>
+        </div>
+    )
+}
+export default Splash;
